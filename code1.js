2cf4ca242e1117bb5659de486b41c5c17faef8e3
aarsha-lune <aarshasreekumarsreekumar@gmail.com>
2025-04-17 14:41:47 +0530

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,5 @@
+[
+    { "title": "The Explorer's Guide", "author": "A. Rivers", "year": 2021, "genre": "Adventure" },
+    { "title": "Mystery Manor", "author": "L. Holmes", "year": 2022, "genre": "Mystery" },
+    { "title": "Digital Dreams", "author": "K. Byte", "year": 2020, "genre": "Sci-Fi" }
+]
